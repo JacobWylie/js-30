cb5855fc51a3d849365a0b6f754fca358c1c35fc
JacobWylie <jacobwylie@gmail.com>
2017-05-03 16:21:40 +0200

    cornify konami code

--- 12-Key-sequence-detection/script.js
+++ 12-Key-sequence-detection/script.js
@@ -1,1 +1,14 @@
-script.js
+const pressed = [];
+const secretCode = 'cornify';
+
+window.addEventListener('keyup', (e) => {
+	console.log(e.key);
+	pressed.push(e.key);
+	pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+	console.log(pressed);
+
+	if (pressed.join('').includes(secretCode)) {
+		cornify_add();
+	};
+});
+
